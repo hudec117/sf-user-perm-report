819098be749cec175889f4a7fae804958c418f9d
Aurel Hudec <hudec117@gmail.com>
2021-02-27 14:05:09 +0000

    Added summary text to items on field permissions and other metadata types.

--- src/front-end/components/FieldPermissionsItemTable.vue
+++ src/front-end/components/FieldPermissionsItemTable.vue
@@ -29,6 +29,14 @@
                         <b-icon-dash scale="1.5" v-else></b-icon-dash>
                     </template>
 
+                    <template #cell(summary)="row">
+                        <span v-for="permissionName of permissionNames"
+                                :key="permissionName"
+                                :class="permissionSummaryBadgeClasses(row.item, permissionName)">
+                            {{ permissionName | sentenceCase }}
+                        </span>
+                    </template>
+
                     <template #row-details="row">
                         <div class="pl-4">
                             <permission-table :permissions="row.item.permissions"></permission-table>
@@ -42,6 +50,8 @@
 
 <script>
     import PermissionTable from './PermissionTable.vue';
+
+    const CHECKBOX_PERMISSIONS = ['default', 'visible', 'enabled', 'editable', 'readable', 'allowCreate', 'allowDelete', 'allowEdit', 'allowRead', 'modifyAllRecords', 'viewAllRecords'];
 
     export default {
         components: { PermissionTable },
@@ -60,7 +70,8 @@
                         key: 'show_details',
                         tdClass: 'collapse-cell-inner'
                     },
-                    'name'
+                    'name',
+                    'summary'
                 ]
             };
         },
@@ -96,11 +107,44 @@
                 }
 
                 return Array.from(objectLookup.values());
+            },
+            permissionNames: function() {
+                const allFieldNames = Object.keys(this.items[0].permissions[0]);
+
+                const checkboxFieldNames = allFieldNames.filter(field => CHECKBOX_PERMISSIONS.includes(field))
+
+                return checkboxFieldNames;
+            }
+        },
+        filters: {
+            sentenceCase: function(value) {
+                if (!value) return '';
+
+                const result = value.replace(/([A-Z])/g, ' $1');
+                return result.charAt(0).toUpperCase() + result.slice(1);
             }
         },
         methods: {
             onRowClick: function(item) {
                 item._showDetails = !item._showDetails;
+            },
+            permissionSummaryBadgeClasses: function(item, permissionName) {
+                let positive = false;
+
+                for (const row of item.permissions) {
+                    if (row[permissionName] == 'true') {
+                        positive = true;
+                    }
+                }
+
+                let baseClasses = 'badge badge-pill mr-2';
+                if (positive) {
+                    baseClasses += ' badge-success';
+                } else {
+                    baseClasses += ' badge-danger';
+                }
+
+                return baseClasses;
             }
         }
     };
--- src/front-end/components/StandardItemTable.vue
+++ src/front-end/components/StandardItemTable.vue
@@ -14,6 +14,14 @@
             <b-icon-dash scale="1.5" v-else></b-icon-dash>
         </template>
 
+        <template #cell(summary)="row">
+            <span v-for="permissionName of permissionNames"
+                  :key="permissionName"
+                  :class="permissionSummaryBadgeClasses(row.item, permissionName)">
+                {{ permissionName | sentenceCase }}
+            </span>
+        </template>
+
         <template #row-details="row">
             <div class="pl-4">
                 <permission-table :permissions="row.item.permissions"></permission-table>
@@ -25,6 +33,8 @@
 <script>
     import PermissionTable from './PermissionTable.vue';
 
+    const CHECKBOX_PERMISSIONS = ['default', 'visible', 'enabled', 'editable', 'readable', 'allowCreate', 'allowDelete', 'allowEdit', 'allowRead', 'modifyAllRecords', 'viewAllRecords'];
+
     export default {
         components: { PermissionTable },
         props: ['items'],
@@ -35,18 +45,52 @@
                         key: 'show_details',
                         tdClass: 'collapse-cell'
                     },
-                    'name'
+                    'name',
+                    'summary'
                 ]
             };
         },
         computed: {
             visibleItems: function() {
                 return this.items.filter(item => item._visible);
+            },
+            permissionNames: function() {
+                const allFieldNames = Object.keys(this.items[0].permissions[0]);
+
+                const checkboxFieldNames = allFieldNames.filter(field => CHECKBOX_PERMISSIONS.includes(field))
+
+                return checkboxFieldNames;
+            }
+        },
+        filters: {
+            sentenceCase: function(value) {
+                if (!value) return '';
+
+                const result = value.replace(/([A-Z])/g, ' $1');
+                return result.charAt(0).toUpperCase() + result.slice(1);
             }
         },
         methods: {
             onRowClick: function(item) {
                 item._showDetails = !item._showDetails;
+            },
+            permissionSummaryBadgeClasses: function(item, permissionName) {
+                let positive = false;
+
+                for (const row of item.permissions) {
+                    if (row[permissionName] == 'true') {
+                        positive = true;
+                    }
+                }
+
+                let baseClasses = 'badge badge-pill mr-2';
+                if (positive) {
+                    baseClasses += ' badge-success';
+                } else {
+                    baseClasses += ' badge-danger';
+                }
+
+                return baseClasses;
             }
         }
     };
